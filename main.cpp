359cb65a99309c5076970be8b8207d4bbc9bccd7
danielzinoviev@yandex.ru <danielzinoviev@yandex.ru>
2024-08-28 22:46:14 +0300

    class Person

--- main.cpp
+++ main.cpp
@@ -56,8 +56,66 @@ class Database
             return false;
         }
 
+        double getBalance(const std::string& phone_number) {
+            try
+            {
+                pqxx::work txn(_connection);
+                pqxx::result result = txn.exec("SELECT balance FROM users WHERE phone_number = " + txn.quote(phone_number));
+
+                if (result.size() > 0)
+                {
+                    return result[0][0].as<double>();
+                }
+                else
+                {
+                    throw std::runtime_error("Пользователь не найден.");
+                }
+            }
+            catch (const std::exception &e)
+            {
+                std::cerr << "Ошибка получения баланса: " << e.what() << std::endl;
+            }
+            return 0.0;
+        }
+
+        void updateBalance(const std::string& phone_number, double amount)
+        {
+            try
+            {
+                pqxx::work txn(_connection);
+                txn.exec0("UPDATE users SET balance = balance + " + txn.quote(amount) + " WHERE phone_number = " + txn.quote(phone_number));
+                txn.commit();
+                std::cout << "Баланс успешно обновлен." << std::endl;
+            }
+            catch (const std::exception &e)
+            {
+                std::cerr << "Ошибка при обновлении баланса: " << e.what() << std::endl;
+            }
+        }
+
     private:
         pqxx::connection& _connection;
+};
+
+class Person {
+
+    public:
+        Person(Database& db, const std::string& phone_number) : _db(db), _phone_number(phone_number) {}
+
+        void displayBalance()
+        {
+            double balance = _db.getBalance(_phone_number);
+            std::cout << "Текущий баланс: " << balance << std::endl;
+        }
+
+        void updateBalance(double amount)
+        {
+            _db.updateBalance(_phone_number, amount);
+        }
+
+    private:
+        Database& _db;
+        std::string _phone_number;
 };
 
 int main()
@@ -65,7 +123,7 @@ int main()
     try {
         // добавьте свои данные для подключения к бд
         //(также понадобится таблица users с id(PK), username, password, balance)
-        pqxx::connection DB("dbname=# user=# password=#");
+        pqxx::connection DB("dbname=bank_app_db user=daniil2 password=7769649Zin");
 
         if (DB.is_open()) {
             std::cout << "Соединение с базой данных установлено." << std::endl;
@@ -105,20 +163,28 @@ int main()
 
                     if (database.checkUser(phone_number))
                     {
+                        Person user(database, phone_number);
                         std::cout << "Введите пароль: ";
                         std::cin >> password;
                         while (true)
                         {
                             std::cout << "Добро пожаловать в личный кабинет." << std::endl;
                             std::cout << "Меню команд:" << std::endl;
-                            std::cout << "1 - Пополнить баланс" << std::endl;
-                            std::cout << "2 - Перевести деньги" << std::endl;
+                            std::cout << "1 - Показать баланс" << std::endl;
+                            std::cout << "2 - Пополнить баланс" << std::endl;
                             std::cout << "0 - Выход" << std::endl;
                             std::cout << "Введите команду: ";
                             std::cin >> choice;
                             if (choice == 1)
                             {
-
+                                user.displayBalance();
+                            }
+                            else if (choice == 2)
+                            {
+                                int amount;
+                                std::cout << "Введите сумму для пополнения: ";
+                                std::cin >> amount;
+                                user.updateBalance(amount);
                             }
                         }
                     }
